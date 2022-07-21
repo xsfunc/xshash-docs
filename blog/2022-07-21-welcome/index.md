7e6038b92a911c936f7a320d812ddb3333616eb2
xsfunc <xsfunc@gmail.com>
2022-07-21 21:46:16 +0300

    initial

---
+++ blog/2022-07-21-welcome/index.md
@@ -0,0 +1,25 @@
+---
+slug: nft-path
+title: 'NFT: Path' 
+authors: [xsfunc]
+tags: [nft, fxhash]
+---
+
+Path — my first collection of 128 editions on [fxhash.xyz](https://www.fxhash.xyz/generative/slug/path)
+
+It consists of tree compositions:
+- Forest
+- Desert
+- Bridge with train
+
+Each composition has few common features like time of day, weather, etc and rare features: rainbow and eclipse. By the way, only one edition has rainbow.
+
+![Path #103](./path103.png)
+
+Usually artists use libraries like [p5.js](https://p5js.org) but I went the other ~~path~~ way. In my first work I try to draw nature with svg primitives like circle, square, line (path in svg) and filters like blur. I deliberately didn't use complex mathematical calculations and algorithms.
+
+Why did I do this? Because I didn't study math at school 😆. I'm most likely not alone in this regard. So, I planning to create some util packages and simple tutorials for community. So that everyone can try themselves as an artist without delving into complex algorithms.
+
+Another plan is `utility` for Path collecton. I created [some tools](http://fxhash.netlify.app/) for `fx(hash)`, you know. And I will create more unique and useful functions for holders of my collection.
+
+In the meantime, follow me on [twitter](https://twitter.com/xsfunc) and stay tuned 😉.
---
+++ docusaurus.config.js
@@ -0,0 +1,126 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'xsHash',
+  tagline: 'Tools for fxhash.xyz',
+  url: 'https://your-docusaurus-test-site.com',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  // organizationName: 'facebook', // Usually your GitHub org/user name.
+  // projectName: 'docusaurus', // Usually your repo name.
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: false,
+
+        // {
+        //   sidebarPath: require.resolve('./sidebars.js'),
+        //   // Please change this to your repo.
+        //   // Remove this to remove the "edit this page" links.
+        //   // editUrl:
+        //   // 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        // },
+        blog: {
+          routeBasePath: '/',
+          showReadingTime: true,
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          // editUrl:
+          // 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'xsHash',
+        logo: {
+          alt: 'xsHash',
+          src: 'img/logo.png',
+        },
+        items: [
+          // {
+          //   type: 'doc',
+          //   docId: 'intro',
+          //   position: 'left',
+          //   label: 'Tutorials',
+          // },
+          { to: '/', label: 'Posts', position: 'left' },
+          // {
+          //   href: 'https://github.com/facebook/docusaurus',
+          //   label: 'GitHub',
+          //   position: 'right',
+          // },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Pages',
+            items: [
+              {
+                label: 'Blog',
+                to: '/blog',
+              },
+            ],
+          },
+          {
+            title: 'Social links',
+            items: [
+              {
+                label: 'Twitter',
+                href: 'https://twitter.com/xsfunc',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'fxhash profile',
+                href: 'https://fxhash.xyz/u/xsfunc',
+              },
+              {
+                label: 'xsHash tools',
+                href: 'https://fxhash.netlify.app/',
+              },
+            ],
+          },
+        ],
+        // copyright: `Copyright © ${new Date().getFullYear()}`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
